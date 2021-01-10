358f5f545c6b502515e9ae41def6d025a3040042
lbin <email@example.com>
2021-01-10 16:09:59 +0800

    错误处理和资源管理补充

--- errhandling/filelistingserver/web.go
+++ errhandling/filelistingserver/web.go
@@ -2,16 +2,40 @@ package main
 
 import (
 	"gostudy/errhandling/filelistingserver/filelisting"
+	"log"
 	"net/http"
 	"os"
 )
+
+type userError interface {
+	error
+	Message() string
+}
 
 type appHandler func(writer http.ResponseWriter, request *http.Request) error
 
 func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			r := recover()
+			if r != nil {
+				log.Printf("Painc: %v", r)
+				http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+				return
+			}
+		}()
+
 		err := handler(writer, request)
+
 		if err != nil {
+
+			log.Printf("Error occurred handling request: %s", err.Error())
+
+			if err, ok := err.(userError); ok {
+				http.Error(writer, err.Message(), http.StatusInternalServerError)
+				return
+			}
+
 			code := http.StatusOK
 			switch {
 			case os.IsNotExist(err):
@@ -25,7 +49,7 @@ func errWrapper(handler appHandler) func(http.ResponseWriter, *http.Request) {
 }
 
 func main() {
-	http.HandleFunc("/list/", errWrapper(filelisting.WriteFile))
+	http.HandleFunc("/", errWrapper(filelisting.WriteFile))
 	err := http.ListenAndServe(":8888", nil)
 	if err != nil {
 		panic(err)
