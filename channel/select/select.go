239cb945cedc75de4368bb22ab664563ad153a60
lbin <email@example.com>
2021-01-12 15:58:11 +0800

    select

---
+++ channel/select/select.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func generator() chan int {
+	out := make(chan int)
+	i := 0
+	go func() {
+		for {
+			time.Sleep(time.Duration(rand.Intn(1500)) * time.Millisecond)
+			out <- i
+			i++
+		}
+	}()
+	return out
+}
+
+func worker(id int, c chan int) {
+	for n := range c {
+		fmt.Printf("Worker %d recevied %d\n", id, n)
+		time.Sleep(time.Second)
+	}
+}
+
+func createWorker(id int) chan<- int {
+	c := make(chan int)
+	go worker(id, c)
+	return c
+}
+
+func main() {
+	var c1, c2 = generator(), generator()
+	w := createWorker(0)
+	var values []int
+	tm := time.After(10 * time.Second)
+	tick := time.Tick(time.Second)
+	for {
+		var activeWorker chan<- int
+		var activeValue int
+		if len(values) > 0 {
+			activeWorker = w
+			activeValue = values[0]
+		}
+		select {
+		case n := <-c1:
+			values = append(values, n)
+		case n := <-c2:
+			values = append(values, n)
+		case activeWorker <- activeValue:
+			values = values[1:]
+		case <-time.After(800 * time.Millisecond):
+			fmt.Println("timeout")
+		case <-tick:
+			fmt.Println("queue len = ", len(values))
+		case <-tm:
+			fmt.Println("bye")
+			return
+		}
+	}
+}
